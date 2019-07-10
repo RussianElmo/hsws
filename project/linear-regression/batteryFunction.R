9a987e67680d351d03a4a623e38258962232d78c
Ryan <rpking180@gmail.com>
2019-07-10 15:34:57 -0400

    fixed stuff

--- project/linear-regression/batteryFunction.R
+++ project/linear-regression/batteryFunction.R
@@ -1,6 +1,21 @@
 #finds elements of B corresponding to element A, need to find specific heat later
+Li = "Li"
+Ca = "Ca"
+Mg = "Mg"
+Y = "Y"
+Zn = "Zn"
 findSpecificHeat = function(elementA){
+
   bIndices = which(battery_data$A == elementA)
-  print(battery_data$B[min(bIndices):max(bIndices)])
-  #specificHeat = subset(elemental_data, )
+  bElements = as.vector(battery_data$B[min(bIndices):max(bIndices)])
+  specificHeat = as.vector(subset(elemental_data, Symbol %in% bElements, select = c("Specific Heat", "Symbol"), drop = TRUE))
+  specificHeat = (data.frame(specificHeat))
+  #specificHeat= (as.vector(unlist(specificHeat[1])))
+  print(specificHeat)
+  betterData = merge(battery_data, specificHeat, by.Symbol, by.B)
+  #View(betterData)
+  
+  
+  
+  
 }
